4978503d4d040318ec11043ec8628c1550ba23ff
Krishna Kumar Sinha <70506241+kksinha78@users.noreply.github.com>
2024-12-05 16:53:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import os
+import langchain
 from langchain_groq import ChatGroq
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.embeddings import OllamaEmbeddings
-from langchan.text_splitter import RescursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain  # used for RAG Q&A application
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain # used for RAG Q&A application
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain # used for RAG Q&A application
 from langchain_community.vectorstores import FAISS
-from langchain_community.documents_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 #lets load all environment variables
 from dotenv import load_dotenv
@@ -16,5 +18,56 @@ load_dotenv()
 
 #Load the GROQ api key
 os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+groq_api_key = os.getenv('GROQ_API_KEY')
+llm = ChatGroq(groq_api_key = groq_api_key,model_name = "llama3-8b-8192")
+prompt= ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    <context>
+    Question:{input}
+    """
+)
+
+#Create vector
+def create_vector_embedding(): # we will use session state becuase this vector store will be used for other prpgrames as well.This will ensue our specific Q&A in this app will have sepaerate memory in vector db
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=OpenAIEmbeddings()
+        st.session_state.loader=PyPDFDirectoryLoader("StudyDocs") # data ingestion
+        st.session_state.docs=st.session_state.loader.load() # Document Loading
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size =1000,chunk_overlap = 200)
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:10]) #split starting 10 documents
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embedding)
+
+user_prompt = st.text_input("Enter your query from the research paper")
+
+if st.button("Document Embedding"):
+    create_vector_embedding()
+    st.write("Vector Database is ready")
+
+import time
+
+if user_prompt:
+    document_chain = create_stuff_documents_chain(llm,prompt) #"create_stuff_documents_chain" create a list of documents to passing to model.In {context} all documents will go and whatever specific question input you are ging that will go into {input}
+    retriver =  st.session_state.vectors.as_retriver() # retriver acts a interface between vectorstore db and user
+    retrieval_chain = create_retrieval_chain(retriver,document_chain)
+
+    start = time.process_time()
+    response = retrieval_chain.invoke({"input":user_prompt})
+    print(f"Response time : {time.process_time() - start}")
+
+    st.write(response['answer'])
+
+    ## with a streamlit expander
+    with st.expander("Document similarity search"):
+        for i,doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write("-------")
+
+
+
 
 
